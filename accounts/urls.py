c4cca400172981ca65c8bab92d88b3d4d5aefa92
romeeeoo <tarassenko@me.com>
2023-04-22 10:45:51 +0300

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from accounts.views import RegisterView, LoginView, UserDetailView
+from accounts.views import RegisterView, LoginView, UserDetailView, logout_view
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", logout_view, name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
     path("api-login/", obtain_auth_token, name='api_token_auth'),
     path("accout/<int:pk>/", UserDetailView.as_view(), name="user_detailed")
+
 ]
--- accounts/views/__init__.py
+++ accounts/views/__init__.py
@@ -2,3 +2,4 @@ from .helpers import SuccessUrlMixin
 from .register import RegisterView
 from .login import LoginView
 from .user_detailed import UserDetailView
+from .logout import logout_view
